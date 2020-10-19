ce672f762668a12ba5bc889f945576dd3f5fa3ef
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2020-10-19 19:04:19 +0530

    Add vulnerable github.com/slackhq/nebula@v1.1.0

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/arajkumar/hello-vuln
 
 go 1.14
 
-require github.com/unknwon/cae v1.0.0
+require (
+	github.com/slackhq/nebula v1.1.0
+	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/crypto v0.0.0-20200220183623-bac4c82f6975 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import "fmt"
-import _ "github.com/unknwon/cae"
-import _ "github.com/unknwon/cae/zip"
+
+// import _ "github.com/unknwon/cae"
+// import _ "github.com/unknwon/cae/zip"
+import _ "github.com/slackhq/nebula/cert"
+
+// import _ "github.com/slackhq/nebula"
 
 func main() {
 	fmt.Println("vim-go")
