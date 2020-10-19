364c20e0ed221115c143c731d540637252070426
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2020-10-19 17:23:18 +0530

    import: golang vuln demo

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/arajkumar/hello-vuln
+
+go 1.14
+
+require github.com/unknwon/cae v1.0.0
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+import _ "github.com/unknwon/cae"
+import _ "github.com/unknwon/cae/zip"
+
+func main() {
+	fmt.Println("vim-go")
+}
