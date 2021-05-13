2d4a84a435bb83ff9d41f5b91c92498fd970867d
Arunprasad Rajkumar <arajkuma@redhat.com>
2021-05-13 17:28:19 +0530

    test vuln

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-// import _ "github.com/unknwon/cae"
-// import _ "github.com/unknwon/cae/zip"
-import _ "github.com/slackhq/nebula/cert"
+	// import _ "github.com/unknwon/cae"
+	// import _ "github.com/unknwon/cae/zip"
 
-import _ "github.com/slackhq/nebula"
-import _ "code.cloudfoundry.org/archiver/extractor"
-import _ "github.com/googleapis/gax-go"
-import _ "github.com/googleapis/gax-go/v2"
+	_ "aahframe.work"
+
+	_ "github.com/slackhq/nebula/cert"
+
+	_ "github.com/slackhq/nebula"
+
+	_ "code.cloudfoundry.org/archiver/extractor"
+
+	_ "github.com/googleapis/gax-go"
+
+	_ "github.com/googleapis/gax-go/v2"
+)
 
 func main() {
 	fmt.Println("vim-go")
